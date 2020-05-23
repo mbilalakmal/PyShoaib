c80dfd9dd93694e1476c82db3d4924c01c7058b4
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-05-23 05:20:35 +0500

    Basic getter setup

--- main.py
+++ main.py
@@ -1,28 +1,99 @@
-# import firebase_admin
 import time
 from threading import Thread
 from flask import Flask, request, escape, render_template
 from flask_sockets import Sockets
-# from firebase_admin import credentials
-# from firebase_admin import firestore
 # Local Imports
 from schedule import Schedule
 from resources import Resources
 from parameters import Parameters
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
+# App settings
 app = Flask(__name__)
 sockets = Sockets(app)
 
-# Use a service account
-# firebase_admin.initialize_app()
+# Global Variables
+generating = False  # Currently busy
+generated = False  # Waiting for user to get
+timetable_progress = 0  # Current timetable progress
 
-# db = firestore.client()
 
-# Websockets
-@sockets.route('/chat')
-def chat_socket(ws):
+@app.route('/generating')
+def get_generating():
+    return str(generating)
+
+
+def generate_in_background(value):
+    # do something that takes a long time
+    time.sleep(value)
+    timetable_progress += 25
+    print(timetable_progress)
+    time.sleep(value)
+    timetable_progress += 25
+    print(timetable_progress)
+    time.sleep(value)
+    timetable_progress += 25
+    print(timetable_progress)
+    time.sleep(value)
+    timetable_progress += 25
+    print(timetable_progress)
+    generating = False
+    generated = True
+
+
+@app.route('/generate-timetable', methods=['POST'])
+def generate_timetable():
+    if generating:
+        return {
+            code: 300,
+            message: 'Generating timetable...'
+        }
+    if generated:
+        return {
+            code: 301,
+            message: 'Timetable has been generated and is waiting to be downloaded.'
+        }
+    # TODO: Get request body and pass in function
+    request_json = request.get_json(silent=True)
+    if request_json is None:
+        return {
+            code: 400,
+            message: 'The mimetype does not indicate JSON (application/json).'
+        }
+    thread = Thread(
+        target=generate_in_background,
+        kwargs={
+            'value': request.args.get('value', 5)
+        }
+    )
+    thread.start()
+    # Succesfully started generating
+    generating = True
+    return {
+        code: 201,
+        message: 'Started generating timetable.'
+    }
+
+# HTTP endpoint to get the final timetable
+@app.route('/get-timetable')
+def get_final_timetable():
+    if generating:
+        return {
+            code: 300,
+            message: 'Generating timetable...'
+        }
+    if generated:
+        return {
+            code: 200,
+            message: 'Timetable attached.'
+        }
+    return {
+        code: 404,
+        message: 'Could not find a generated timetable.'
+    }
+
+# WS endpoint for getting current progress
+@sockets.route('/get-timetable')
+def get_timetable(ws):
     while not ws.closed:
         message = ws.receive()
         if message is None:  # message is "None" if the client has closed.
@@ -32,40 +103,28 @@ def chat_socket(ws):
         # extra-instance storage or messaging system would be required.)
         clients = ws.handler.server.clients.values()
         for client in clients:
-            client.ws.send(message)
+            client.ws.send(message + str(timetable_progress))
 
 
-# @app.route('/', methods=['POST'])
-# def generate_timetable():
-#     # Background Example
-#     def do_work(value):
-#         # do something that takes a long time
-#         time.sleep(value)
-#         print("Background function completed")
-#     thread = Thread(target=do_work, kwargs={
-#                     'value': request.args.get('value', 5)})
-#     thread.start()
-#     # Firestore example
-#     users_ref = db.collection(u'constraints')
-#     docs = users_ref.stream()
-#     for i in range(50):
-#         print(i)
-#     # for doc in docs:
-#     #     print(u'{} => {}'.format(doc.id, doc.to_dict()))
-#     # Request example
-#     request_json = request.get_json(silent=True)
-#     request_args = request.args
-#     if request_json and 'name' in request_json:
-#         name = request_json['name']
-#     elif request_args and 'name' in request_args:
-#         name = request_args['name']
-#     else:
-#         name = 'World'
-#     return 'Hello {}!'.format(escape(name))
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/delete-timetable')
+def delete_timetable():
+    if generating:
+        # TODO: Stop generating
+        return {
+            code: 300,
+            message: 'Generating timetable...'
+        }
+    if generated:
+        generated = False
+        timetable_progress = 0
+        return {
+            code: 203,
+            message: 'Deleted timetable. You can now generate a new timetable.'
+        }
+    return {
+        code: 404,
+        message: 'No timetable has been generated yet.'
+    }
 
 
 if __name__ == '__main__':
@@ -77,7 +136,5 @@ if __name__ == '__main__':
             This can not be run directly because the Flask development server does not
             support web sockets. Instead, use gunicorn:
             gunicorn -b 127.0.0.1:8080 -k flask_sockets.worker main:app
-            Ensure that you have exported the credentials for Firebase:
-            export GOOGLE_APPLICATION_CREDENTIALS=./serviceAccount.json
     """)
 # [END gae_python37_app]
