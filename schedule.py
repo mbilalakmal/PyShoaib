8645697f358b7911e78e2ab221debb826cde2964
Bilal Akmal <bilalakmal@hotmail.com>
2020-05-27 00:58:04 +0500

    Mutation and Fitness functions added

--- schedule.py
+++ schedule.py
@@ -23,13 +23,14 @@ import random
 
 @total_ordering
 class Schedule:
-    '''
+    """
     Represents a weekly schedule.
 
     Requires a `Resources` object and a `Parameters` object.
-    '''
+    """
+
     def __init__(self, resources: Resources, parameters: Parameters):
-        
+
         # fitness representing constraints fulfilled [0.0 - 1.0]
         self.fitness = 0.0
 
@@ -45,173 +46,128 @@ class Schedule:
             columns=['day', 'hour', 'room_id', 'lecture_id']
         )
 
-
     def initialize(self):
-        '''
+        """
         Assign random room and time slots to each lecture.
 
         Constraints are not considered in this step.
-        '''
-        pass
-        # for each lecture*duration assign a row in entries
+        """
         for lecture in self.resources.lectures.values():
-            print(lecture)
-
             self._assign_lecture(lecture)
 
-        # turn on dirty bit
-        self.dirty_bit = True
-
+        self.dirty_bit = True  # indicate current fitness is obsolete
 
     def mutate(self):
-        '''
-        Reassign room and time slots to a (small) subset of lecture.
+        """
+        Reassign room and time slots of a (small) subset of lecture.
 
         Fitness of the schedule must be re-evaluated after this step.
-        '''
-        pass
-        # for MSIZE*lectures modify a row in entries
+        """
+        sample = int(self.parameters.mutation_size * len(self.resources.lectures))
+        target_ids = random.sample(self.resources.lectures.keys(), sample)
+        lectures = [self.resources.lectures[target_id] for target_id in target_ids]
 
-        # turn on dirty bit
-        self.dirty_bit = True
+        for lecture in lectures:
+            self._remove_lecture(lecture)
+            self._assign_lecture(lecture)
 
+        self.dirty_bit = True  # indicate current fitness is obsolete
 
     def crossover(self, parent1: 'Schedule', parent2: 'Schedule'):
-        '''
+        """
         Copy assigned room and time slots from `parent1` and `parent2`.
 
         Fitness of the schedule must be re-evaluated after this step.
-        '''
+        """
         pass
-        # copy half rows(lecture-wise) from each parent
 
-        # turn on dirty bit
-        self.dirty_bit = True
-
+        self.dirty_bit = True  # indicate current fitness is obsolete
 
     def copy(self, parent: 'Schedule'):
-        '''
+        """
         Copy assigned room and time slots from `parent`.
 
         Fitness is equal to the fitness of `parent`.
-        '''
+        """
         pass
         # copy the whole dataframe and fitness
 
         self.fitness = parent.fitness
 
-
     def calculate_fitness(self):
-        '''
+        """
         Checks each constraints and assigns score based on fulfilled.
-        '''
-        pass
-
-        # check if fitness needs to be evaluated
-        if self.dirty_bit == False:
+        """
+        if not self.dirty_bit:
             return
-
-        # calculate fitness
-
-        import time
-        # 1. Pauli Exclusion
-        a = time.time()
-        # count unique room and time slots
-        # unique_slots = self.entries.groupby(
-        #     ['day', 'hour', 'room_id']
-        #     ).count()
-
-        # unique_slots = len(self.entries.groupby(
-        #     ['day', 'hour', 'room_id']
-        #     ))
-
-        unique_slots = self.entries.groupby(
-            ['day', 'hour', 'room_id']
-            ).ngroups
-
-        c = time.time()
 
         total_slots = len(self.entries.index)
 
-        b = time.time()
+        # 1. Pauli Exclusion
+        unique_slots = self.entries.groupby(
+            ['day', 'hour', 'room_id']
+        ).count()
+        unique_slots = unique_slots[unique_slots == 1].sum()[0]
 
-        print('unique', unique_slots)
-        print('total', total_slots)
-        print(b-a, c-a)
-
-        # 2. capacity
-
-        capacity_slots = 0
-
+        capacity_rooms = 0
         course_slots = 0
         course_rooms = 0
-
         teacher_slots = 0
         teacher_rooms = 0
-
         lecture_slots = 0
 
-        a = time.time()
-        for row in self.entries.itertuples(index=False, name=None):
-            # print(row)
+        for _, row in self.entries.iterrows():
             day, hour, room_id, lecture_id = row
-            lecture = self.resources.lectures[lecture_id]
+
             room = self.resources.rooms[room_id]
-            if room.capacity >= lecture.strength:
-                capacity_slots += 1
-
+            lecture = self.resources.lectures[lecture_id]
             course = self.resources.courses[lecture.course_id]
-            if course.available_slots[day][hour] == True:
-                course_slots += 1
-            if room_id in course.available_room_ids:
-                course_rooms += 1
 
-            teachers = [
-                self.resources.teachers[t_id] for t_id in lecture.teacher_ids
-            ]
+            teachers = [self.resources.teachers[t_id] for t_id in lecture.teacher_ids]
 
-            nteachers = len(teachers)
+            concurrent_l_ids = set(
+                self.entries.loc[
+                    (self.entries['day'] == day) &
+                    (self.entries['hour'] == hour)
+                ]['lecture_id']
+            )
+
+            capacity_rooms += (room.capacity >= lecture.strength)
+            course_slots += course.available_slots[day][hour]
+            course_rooms += (room_id in course.available_room_ids)
 
             for teacher in teachers:
-                if teacher.available_slots[day][hour] == True:
-                    teacher_slots += 1/nteachers
-                if room_id in teacher.available_room_ids:
-                    teacher_rooms += 1/nteachers
+                teacher_slots += (teacher.available_slots[day][hour]) / len(teachers)
+                teacher_rooms += (room_id in teacher.available_room_ids) / len(teachers)
 
-            concurrent_lecture_ids = set(self.entries[
-                (self.entries['day'] == day) &
-                (self.entries['hour'] == hour)
-            ]['lecture_id'])
+            lecture_slots += concurrent_l_ids.isdisjoint(lecture.noncurrent_lecture_ids)
 
-            if concurrent_lecture_ids.isdisjoint(lecture.noncurrent_lecture_ids) == True:
-                lecture_slots += 1
+        print(f'UniqueS: {unique_slots}\n'
+              f'CapacityR: {capacity_rooms}\n'
+              f'CourseS: {course_slots}\n'
+              f'CourseR: {course_rooms}\n'
+              f'TeacherS: {teacher_slots}\n'
+              f'TeacherR: {teacher_rooms}\n'
+              f'LectureS: {lecture_slots}\n')
 
-        b = time.time()
-        print('cap', capacity_slots)
-        print('courses', course_slots)
-        print('courser', course_rooms)
-        print('teachers', teacher_slots)
-        print('teacherr', teacher_rooms)
-        print('lectures', lecture_slots)
-        print(b-a)
+        score = (unique_slots + capacity_rooms + course_slots +
+                 course_rooms + teacher_slots + teacher_rooms + lecture_slots)
+        score /= (total_slots * 7)
+        print(f'score: {score}')
 
-
-
-
-    #----------------------------------------
+    # ----------------------------------------
     # PRIVATE METHODS
-    #----------------------------------------
-
+    # ----------------------------------------
 
     def _assign_lecture(self, lecture: Lecture):
-        '''
+        """
         Assigns room and time slots of `lecture`.
-        '''
+        """
 
         course = self.resources.courses[lecture.course_id]
         duration = course.duration
 
-        if course.is_lab_course == True:
+        if course.is_lab_course:
             slots = self._get_random_lab_slots(duration)
         else:
             slots = self._get_random_theory_slots(duration)
@@ -223,13 +179,12 @@ class Schedule:
         # slots is a list of lists
         self.entries = self.entries.append(
             pd.DataFrame(slots, columns=self.entries.columns)
-            )
-
+        )
 
     def _get_random_lab_slots(self, duration):
-        '''
+        """
         Returns `duration` room and time slots for a lab.
-        '''
+        """
         slots = []
 
         day = random.choice(
@@ -245,16 +200,15 @@ class Schedule:
         )
 
         for i in range(duration):
-            slot = [day, hour+i, room_id]
+            slot = [day, hour + i, room_id]
             slots.append(slot)
 
         return slots
 
-
     def _get_random_theory_slots(self, duration):
-        '''
+        """
         Returns `duration` room and time slots for a theory.
-        '''
+        """
         slots = []
 
         days = random.sample(
@@ -275,14 +229,17 @@ class Schedule:
 
         return slots
 
+    def _remove_lecture(self, lecture: Lecture):
+        """
+        Remove room and time slots of `lecture`.
+        """
+        self.entries = self.entries[self.entries['lecture_id'] != lecture.id]
 
-    def __gt__(self, other: 'Chromsome'):
+    def __gt__(self, other: 'Schedule'):
         return self.fitness > other.fitness
 
-
-    def __eq__(self, other: 'Chromsome'):
+    def __eq__(self, other: 'Schedule'):
         return self.fitness == other.fitness
 
-    
     def __repr__(self):
         return f'Fitness: {self.fitness}\n'
