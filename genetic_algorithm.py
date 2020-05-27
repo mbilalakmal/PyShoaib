68e8f7dd20d685a52e3ddc077dc48eb06d322c31
Bilal Akmal <bilalakmal@hotmail.com>
2020-05-27 18:30:22 +0500

    Added ga module

---
+++ genetic_algorithm.py
@@ -0,0 +1,44 @@
+# -----------------------------------------------------------
+# This module represents a GeneticAlgorithm.
+#
+# A genetic algorithm must be provided with:
+# (1) chromosome length,
+# (2) population size,
+# (3) and number of generations
+#
+# After initialization, a single method 'run' will
+# operate to generate new generations until a solution is found.
+#
+#
+# (C) 2020 PyShoaib
+# -----------------------------------------------------------
+
+from schedule import Schedule
+from parameters import Parameters
+
+import pandas as pd
+import numpy as np
+
+
+class GeneticAlgorithm:
+
+    def __int__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def describe(self):
+        pass
+
+    def _initialize(self):
+        pass
+
+    def _reproduce(self):
+        pass
+
+    def _tournament_selection(self):
+        pass
+
+    def _track_best(self):
+        pass
