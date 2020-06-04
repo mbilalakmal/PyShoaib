a13cf58ce3dccbd99c940f10686d0979acceaa41
Bilal Akmal <bilalakmal@hotmail.com>
2020-06-04 17:00:23 +0500

    Added best_schedule preservation in reproduce function

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -37,16 +37,12 @@ class GeneticAlgorithm:
 
     def run(self):
         self._initialize()
-        while (
-                self.optimum_reached is False and
-                self.generation < self.parameters.maximum_generations
-        ):
+        while (not self.optimum_reached) and (self.generation < self.parameters.maximum_generations):
             self._reproduce()
             self.generation += 1
-        return self.optimum_reached
+            print(f'Gen: {self.generation}  Best: {self.best_fitness}')
 
-    def describe(self):
-        pass
+        return self.optimum_reached
 
     def _initialize(self):
         """
@@ -62,7 +58,7 @@ class GeneticAlgorithm:
     def _reproduce(self):
         population = np.empty_like(self._population)
 
-        for idx in range(self.parameters.population_size):
+        for idx in range(self.parameters.population_size - 1):
 
             # crossover
             parent1, parent2 = self._tournament_selection(), self._tournament_selection()
@@ -72,7 +68,7 @@ class GeneticAlgorithm:
 
             child = Schedule(self.resources, self.parameters)
 
-            if np.random.binomial(1, self.parameters.crossover_rate):
+            if np.random.binomial(1, p=self.parameters.crossover_rate):
                 child.crossover(parent1, parent2)
             else:
                 child.copy(np.random.choice([parent1, parent2]))
@@ -85,6 +81,14 @@ class GeneticAlgorithm:
 
             population[idx] = child
 
+        # preserve the best
+        child = Schedule(self.resources, self.parameters)
+        if self.best_schedule is not None:
+            child.copy(self.best_schedule)
+        else:
+            child.initialize()
+        population[-1] = child
+
         self._population = population
         self._track_best()
 
@@ -94,6 +98,6 @@ class GeneticAlgorithm:
 
     def _track_best(self):
         self.best_schedule = np.amax(self._population)
-        self.best_fitness = np.amax(self._population).fitness
+        self.best_fitness = self.best_schedule.fitness
 
         self.optimum_reached = self.best_fitness == 1.0
